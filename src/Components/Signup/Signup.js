9c26236608431490befbaac07c334b72a70a8bcf
mnaofficial <mna.official92@gmail.com>
2022-04-09 00:39:56 +0600

    password and email input

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -9,13 +9,9 @@ const provider = new GoogleAuthProvider();
 
 const Signup = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-
-  console.log(email);
-  console.log(password);
-  console.log(confirmPassword);
+  const [email, setEmail] = useState({ value: "", error: "" })
+  const [password, setPassword] = useState({ value: "", error: "" })
+  const [confirmPassword, setConfirmPassword] = useState({ value: "", error: "" })
 
   const googleAuth = () => {
 
@@ -29,28 +25,52 @@ const Signup = () => {
   }
 
   const handleEmail = (emailInput) => {
-    setEmail(emailInput);
+    if (/\S+@\S+\.\S+/.test(emailInput)) {
+      setEmail({ value: emailInput, error: "" });
+    }
+    else {
+      setEmail({ value: "", error: "Invalid Email" });
+    }
   }
   const handlePassword = (passwordInput) => {
     setPassword(passwordInput);
+    if (passwordInput.length < 7) {
+      setPassword({ value: "", error: "Password too short" });
+    }
+    else {
+      setPassword({ value: passwordInput, error: "" });
+    }
   }
-  const handleConfirmPassword = (conFirmPasswordInput) => {
-    setConfirmPassword(conFirmPasswordInput);
+  const handleConfirmPassword = (confirmPasswordInput) => {
+    if (confirmPasswordInput === password.value) {
+      setConfirmPassword({ value: confirmPasswordInput, error: "" });
+    }
+    else{
+      setConfirmPassword({ value: "", error: "Password Mismatched" });
+
+    }
   }
 
   const handleSignup = (event) => {
     event.preventDefault()
-    const email = event.target.email.value;
-    const password = event.target.password.value;
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      });
+    if (email.value === "") {
+      setEmail({ value: "", error: "Email is require" })
+    }
+    if (password.value === "") {
+      setPassword({ value: "", error: "Password is require" })
+    }
+
+    if (email.value && password.value && confirmPassword.value===password.value) {
+      createUserWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+          const user = userCredential.user;
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    }
   }
 
   return (
@@ -61,20 +81,23 @@ const Signup = () => {
           <div className='input-field'>
             <label htmlFor='email'>Email</label>
             <div className='input-wrapper'>
-              <input type='text' name='email' id='email' onBlur={(event)=>handleEmail(event.target.value)} />
+              <input type='text' name='email' id='email' onBlur={(event) => handleEmail(event.target.value)} />
             </div>
+            {email?.error && <p className="error">{email.error}</p>}
           </div>
           <div className='input-field'>
             <label htmlFor='password'>Password</label>
             <div className='input-wrapper'>
-              <input type='password' name='password' id='password' onBlur={(event)=>handlePassword(event.target.value)} />
+              <input type='password' name='password' id='password' onBlur={(event) => handlePassword(event.target.value)} />
             </div>
+            {password?.error && <p className="error">{password.error}</p>}
           </div>
           <div className='input-field'>
             <label htmlFor='confirm-password'>Confirm Password</label>
             <div className='input-wrapper'>
-              <input type='password' name='confirmPassword' id='confirm-password' onBlur={(event)=>handleConfirmPassword(event.target.value)} />
+              <input type='password' name='confirmPassword' id='confirm-password' onBlur={(event) => handleConfirmPassword(event.target.value)} />
             </div>
+            {confirmPassword?.error && <p className="error">{confirmPassword.error}</p>}
           </div>
           <button type='submit' className='auth-form-submit'>
             Sign Up
